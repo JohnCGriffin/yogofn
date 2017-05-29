c38919c1415c76832403330588d1f95eced4b78b
griffin <griffinish@gmail.com>
2017-05-29 07:36:00 -0400

    And/Every

--- testdata.go
+++ testdata.go
@@ -33,3 +33,26 @@ var estados = []string{
 	"Yucatán",
 	"Zacatecas",
 }
+
+type persona struct {
+	nombre string
+	edad   int
+}
+
+var adultos = []persona{
+	{"Jose", 33},
+	{"John", 56},
+	{"Isabel", 19},
+	{"Ramón", 39},
+}
+
+var ninos = []persona{
+	{"Becky", 12},
+	{"Javier", 8},
+	{"Shanika", 11},
+	{"Camila", 7},
+	{"Diego", 5},
+	{"Alejandro", 17},
+}
+
+var nums = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
